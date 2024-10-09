fb3aa31ae1e4a347b4109186c3b215f7149d9174
Lucas Lu <lucas89901@gmail.com>
2024-10-10 02:05:21 +0800

    Reformat files

--- server.c
+++ server.c
@@ -4,9 +4,10 @@ const unsigned char IAC_IP[3] = "\xff\xf4";
 const char* file_prefix = "./csie_trains/train_";
 const char* accept_read_header = "ACCEPT_FROM_READ";
 const char* accept_write_header = "ACCEPT_FROM_WRITE";
-const char* welcome_banner = "======================================\n"
-                             " Welcome to CSIE Train Booking System \n"
-                             "======================================\n";
+const char* welcome_banner =
+    "======================================\n"
+    " Welcome to CSIE Train Booking System \n"
+    "======================================\n";
 
 const char* lock_msg = ">>> Locked.\n";
 const char* exit_msg = ">>> Client exit.\n";
@@ -57,9 +58,9 @@ int handle_read(request* reqP) {
     r = read(reqP->conn_fd, buf, sizeof(buf));
     if (r < 0) return -1;
     if (r == 0) return 0;
-    char* p1 = strstr(buf, "\015\012"); // \r\n
+    char* p1 = strstr(buf, "\015\012");  // \r\n
     if (p1 == NULL) {
-        p1 = strstr(buf, "\012");   // \n
+        p1 = strstr(buf, "\012");  // \n
         if (p1 == NULL) {
             if (!strncmp(buf, IAC_IP, 2)) {
                 // Client presses ctrl+C, regard as disconnection
@@ -72,13 +73,12 @@ int handle_read(request* reqP) {
     len = p1 - buf + 1;
     memmove(reqP->buf, buf, len);
     reqP->buf[len - 1] = '\0';
-    reqP->buf_len = len-1;
+    reqP->buf_len = len - 1;
     return 1;
 }
 
 #ifdef READ_SERVER
-int print_train_info(request *reqP) {
-    
+int print_train_info(request* reqP) {
     int i;
     char buf[MAX_MSG_LEN];
 
@@ -89,29 +89,29 @@ int print_train_info(request *reqP) {
     return 0;
 }
 #else
-int print_train_info(request *reqP) {
+int print_train_info(request* reqP) {
     /*
      * Booking info
      * |- Shift ID: 902001
      * |- Chose seat(s): 1,2
      * |- Paid: 3,4
      */
-    char buf[MAX_MSG_LEN*3];
+    char buf[MAX_MSG_LEN * 3];
     char chosen_seat[MAX_MSG_LEN] = "1,2";
     char paid[MAX_MSG_LEN] = "3,4";
 
     memset(buf, 0, sizeof(buf));
-    sprintf(buf, "\nBooking info\n"
-                 "|- Shift ID: %d\n"
-                 "|- Chose seat(s): %s\n"
-                 "|- Paid: %s\n\n"
-                 ,902001, chosen_seat, paid);
+    sprintf(buf,
+            "\nBooking info\n"
+            "|- Shift ID: %d\n"
+            "|- Chose seat(s): %s\n"
+            "|- Paid: %s\n\n",
+            902001, chosen_seat, paid);
     return 0;
 }
 #endif
 
 int main(int argc, char** argv) {
-
     // Parse args.
     if (argc != 2) {
         fprintf(stderr, "usage: %s [port]\n", argv[0]);
@@ -119,9 +119,9 @@ int main(int argc, char** argv) {
     }
 
     int conn_fd;  // fd for file that we open for reading
-    char buf[MAX_MSG_LEN*2], filename[FILE_LEN];
+    char buf[MAX_MSG_LEN * 2], filename[FILE_LEN];
 
-    int i,j;
+    int i, j;
 
     for (i = TRAIN_ID_START, j = 0; i <= TRAIN_ID_END; i++, j++) {
         getfilepath(filename, i);
@@ -138,49 +138,46 @@ int main(int argc, char** argv) {
     }
 
     // Initialize server
-    init_server((unsigned short) atoi(argv[1]));
+    init_server((unsigned short)atoi(argv[1]));
 
     // Loop for handling connections
-    fprintf(stderr, "\nstarting on %.80s, port %d, fd %d, maxconn %d...\n", svr.hostname, svr.port, svr.listen_fd, maxfd);
+    fprintf(stderr, "\nstarting on %.80s, port %d, fd %d, maxconn %d...\n", svr.hostname, svr.port, svr.listen_fd,
+            maxfd);
 
     while (1) {
         // TODO: Add IO multiplexing
 
         // Check new connection
         conn_fd = accept_conn();
-        if (conn_fd < 0)
-            continue;
+        if (conn_fd < 0) continue;
 
         int ret = handle_read(&requestP[conn_fd]);
-	    if (ret < 0) {
+        if (ret < 0) {
             fprintf(stderr, "bad request from %s\n", requestP[conn_fd].host);
             continue;
         }
 
         // TODO: handle requests from clients
-#ifdef READ_SERVER      
-        sprintf(buf,"%s : %s",accept_read_header,requestP[conn_fd].buf);
+#ifdef READ_SERVER
+        sprintf(buf, "%s : %s", accept_read_header, requestP[conn_fd].buf);
         write(requestP[conn_fd].conn_fd, buf, strlen(buf));
 #elif defined WRITE_SERVER
-        sprintf(buf,"%s : %s",accept_write_header,requestP[conn_fd].buf);
-        write(requestP[conn_fd].conn_fd, buf, strlen(buf));    
+        sprintf(buf, "%s : %s", accept_write_header, requestP[conn_fd].buf);
+        write(requestP[conn_fd].conn_fd, buf, strlen(buf));
 #endif
-        
 
         close(requestP[conn_fd].conn_fd);
         free_request(&requestP[conn_fd]);
-    }      
+    }
 
     free(requestP);
     close(svr.listen_fd);
-    for (i = 0;i < TRAIN_NUM; i++)
-        close(trains[i].file_fd);
+    for (i = 0; i < TRAIN_NUM; i++) close(trains[i].file_fd);
 
     return 0;
 }
 
 int accept_conn(void) {
-
     struct sockaddr_in cliaddr;
     size_t clilen;
     int conn_fd;  // fd for a new connection with client
@@ -190,24 +187,24 @@ int accept_conn(void) {
     if (conn_fd < 0) {
         if (errno == EINTR || errno == EAGAIN) return -1;  // try again
         if (errno == ENFILE) {
-            (void) fprintf(stderr, "out of file descriptor table ... (maxconn %d)\n", maxfd);
-                return -1;
+            (void)fprintf(stderr, "out of file descriptor table ... (maxconn %d)\n", maxfd);
+            return -1;
         }
         ERR_EXIT("accept");
     }
-    
+
     requestP[conn_fd].conn_fd = conn_fd;
     strcpy(requestP[conn_fd].host, inet_ntoa(cliaddr.sin_addr));
     fprintf(stderr, "getting a new request... fd %d from %s\n", conn_fd, requestP[conn_fd].host);
-    requestP[conn_fd].client_id = (svr.port * 1000) + num_conn;    // This should be unique for the same machine.
+    requestP[conn_fd].client_id = (svr.port * 1000) + num_conn;  // This should be unique for the same machine.
     num_conn++;
-    
+
     return conn_fd;
 }
 
 static void getfilepath(char* filepath, int extension) {
-    char fp[FILE_LEN*2];
-    
+    char fp[FILE_LEN * 2];
+
     memset(filepath, 0, FILE_LEN);
     sprintf(fp, "%s%d", file_prefix, extension);
     strcpy(filepath, fp);
@@ -227,8 +224,7 @@ static void init_request(request* reqP) {
 
     reqP->booking_info.num_of_chosen_seats = 0;
     reqP->booking_info.train_fd = -1;
-    for (int i = 0; i < SEAT_NUM; i++)
-        reqP->booking_info.seat_stat[i] = UNKNOWN;
+    for (int i = 0; i < SEAT_NUM; i++) reqP->booking_info.seat_stat[i] = UNKNOWN;
 }
 
 static void free_request(request* reqP) {
@@ -263,7 +259,7 @@ static void init_server(unsigned short port) {
 
     // Get file descripter table size and initialize request table
     maxfd = getdtablesize();
-    requestP = (request*) malloc(sizeof(request) * maxfd);
+    requestP = (request*)malloc(sizeof(request) * maxfd);
     if (requestP == NULL) {
         ERR_EXIT("out of memory allocating all requests");
     }
--- server.h
+++ server.h
@@ -1,26 +1,30 @@
 #ifndef __SERVER_H
 #define __SERVER_H
 
+#include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/select.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 /*
  * Feel free to edit any part of codes
  */
 
-#define ERR_EXIT(a) do { perror(a); exit(1); } while(0)
+#define ERR_EXIT(a) \
+    do {            \
+        perror(a);  \
+        exit(1);    \
+    } while (0)
 
 #define TRAIN_NUM 5
 #define SEAT_NUM 40
@@ -30,48 +34,48 @@
 #define MAX_MSG_LEN 512
 
 enum STATE {
-    INVALID,    // Invalid state
-    SHIFT,      // Shift selection
-    SEAT,       // Seat selection
-    BOOKED      // Payment
+    INVALID,  // Invalid state
+    SHIFT,    // Shift selection
+    SEAT,     // Seat selection
+    BOOKED    // Payment
 };
 
 enum SEAT {
-    UNKNOWN,    // Seat is unknown
-    CHOSEN,     // Seat is currently being reserved 
-    PAID        // Seat is already paid for
+    UNKNOWN,  // Seat is unknown
+    CHOSEN,   // Seat is currently being reserved
+    PAID      // Seat is already paid for
 };
 
 typedef struct {
-    char hostname[512];  // server's hostname
+    char hostname[512];   // server's hostname
     unsigned short port;  // port to listen
-    int listen_fd;  // fd to wait for a new connection
+    int listen_fd;        // fd to wait for a new connection
 } server;
 
 typedef struct {
-    int shift_id;               // shift id 902001-902005
-    int train_fd;               // train file fds
-    int num_of_chosen_seats;    // num of chosen seats
-    enum SEAT seat_stat[SEAT_NUM];   // seat status
+    int shift_id;                   // shift id 902001-902005
+    int train_fd;                   // train file fds
+    int num_of_chosen_seats;        // num of chosen seats
+    enum SEAT seat_stat[SEAT_NUM];  // seat status
 } record;
 
 typedef struct {
-    char host[512];             // client's host
-    int conn_fd;                // fd to talk with client
-    int client_id;              // client's id
-    char buf[MAX_MSG_LEN];      // data sent by/to client
-    size_t buf_len;             // bytes used by buf
-    enum STATE status;          // request status
-    record booking_info;        // booking status
-    struct timeval remaining_time; // connection remaining time
+    char host[512];                 // client's host
+    int conn_fd;                    // fd to talk with client
+    int client_id;                  // client's id
+    char buf[MAX_MSG_LEN];          // data sent by/to client
+    size_t buf_len;                 // bytes used by buf
+    enum STATE status;              // request status
+    record booking_info;            // booking status
+    struct timeval remaining_time;  // connection remaining time
 } request;
 
 typedef struct {
-    int file_fd;                    // fd of file
+    int file_fd;  // fd of file
 } train_info;
 
 // Global variable
-server svr;  // server
+server svr;                // server
 request* requestP = NULL;  // point to a list of requests
 train_info trains[TRAIN_NUM];
 int maxfd;  // size of open file descriptor table, size of request list
